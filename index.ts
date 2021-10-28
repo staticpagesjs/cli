3c470194385622a7ffd00dcad0eb2ac8b38e615f
root <root@localhost>
2021-10-28 21:35:54 +0200

    changed config format

--- index.ts
+++ index.ts
@@ -12,12 +12,12 @@ program
   .name(Object.keys(pkg.bin)[0])
   .version(pkg.version, '-v, --version', 'output the current cli version')
   .option('-c, --config <path>', 'path to a build configuration file')
-  .option('-r, --from-reader <package>', 'import default from this package as the reader')
+  .option('-r, --from <package>', 'import \'default\' or \'cli\' from this package as the reader')
   .option('-a, --from-args <JSON-string>', 'arguments passed to reader; provide in JSON format')
-  .option('-w, --to-writer <package>', 'import default from this package as the writer')
+  .option('-w, --to <package>', 'import \'default\' or \'cli\' from this package as the writer')
   .option('-A, --to-args <JSON-string>', 'arguments passed to writer; provide in JSON format')
   .option('-s, --controller <package>', 'controller that can process the input data before rendering')
-  .option('-x, --context <JSON-string>', 'additional object that will be passed to the controller as \'this\'')
+  .option('-t, --context <JSON-string>', 'additional object that will be passed to the controller as \'this\'')
   .parse();
 
 /**
@@ -27,10 +27,10 @@ program
  * @param x The variable to check.
  * @param desiredType The desired type.
  */
-const assertType = (name: string, x: any, desiredType: string): void => {
+const assertType = (name: string, x: any, ...desiredType: string[]): void => {
   const actualType = typeof x === 'object' ? (x ? 'object' : 'null') : typeof x;
-  if (actualType !== desiredType)
-    throw new Error(`'${name}' type mismatch, expected '${desiredType}', got '${actualType}'.`);
+  if (!desiredType.includes(actualType))
+    throw new Error(`'${name}' type mismatch, expected '${desiredType.join("', '")}', got '${actualType}'.`);
 };
 
 /**
@@ -46,12 +46,13 @@ const ensureArray = (x: any): unknown[] => Array.isArray(x) ? x : [x];
  * Imports a CommonJS module, relative from the process.cwd().
  *
  * @param file Module path.
+ * @param preferredImport Preferred import, if not exists fallbacks to default, then a cjs function export.
  * @returns Module exports.
  */
-const importCliModule = (file: any): unknown => {
+const importCliModule = (file: any, preferredImport: string = 'cli'): unknown => {
   try {
     const mod: any = importFrom(process.cwd(), file);
-    if (mod.cli) return mod.cli;
+    if (mod[preferredImport]) return mod[preferredImport];
     if (mod.default) return mod.default;
     return mod;
   } catch (error: any) {
@@ -69,27 +70,38 @@ async function prepareRoute(route: any): Promise<Route> {
   const { from, to, controller, ...rest } = route;
 
   assertType('route', route, 'object');
-  assertType('route.from', route.from, 'object');
-  assertType('route.from.reader', route.from.reader, 'string');
-  assertType('route.to', route.to, 'object');
-  assertType('route.to.writer', route.to.writer, 'string');
+  assertType('route.from', route.from, 'object', 'string');
+  assertType('route.to', route.to, 'object', 'string');
+
+  const fromModuleKey = typeof from === 'object' ? 'route.from.module' : 'route.from';
+  const fromModuleName = typeof from === 'object' ? from.module : from;
+  const fromImportName = typeof from === 'object' ? from.import : 'cli';
+  const fromArgs = typeof from === 'object' ? from.args : undefined;
+
+  const toModuleKey = typeof to === 'object' ? 'route.to.module' : 'route.to';
+  const toModuleName = typeof to === 'object' ? to.module : to;
+  const toImportName = typeof to === 'object' ? to.import : 'cli';
+  const toArgs = typeof to === 'object' ? to.args : undefined;
+
+  assertType(fromModuleKey, fromModuleName, 'string');
+  assertType(toModuleKey, toModuleName, 'string');
 
   // Construct the route object accepted by the core.
-  const fromReader = importCliModule(from.reader);
-  if (typeof fromReader !== 'function')
-    throw new Error(`'route.from.reader' of '${from.reader}' does not exports a function.`);
+  const fromFactory = importCliModule(fromModuleName, fromImportName);
+  if (typeof fromFactory !== 'function')
+    throw new Error(`'${fromModuleKey}' of '${fromModuleName}' does not exports a function.`);
 
-  const fromIterable = await fromReader.apply(undefined, ensureArray(from.args));
+  const fromIterable = await fromFactory.apply(undefined, ensureArray(fromArgs));
   if (!(Symbol.iterator in fromIterable || Symbol.asyncIterator in fromIterable))
-    throw new Error(`'route.from.reader' of '${from.reader}' does not provide an iterable or async iterable.`);
+    throw new Error(`'${fromModuleKey}' of '${fromModuleName}' does not provide an iterable or async iterable.`);
 
-  const toWriterInitializer = importCliModule(to.writer);
-  if (typeof toWriterInitializer !== 'function')
-    throw new Error(`'route.to.writer' of '${to.writer}' does not exports a function.`);
+  const toFactory = importCliModule(toModuleName, toImportName);
+  if (typeof toFactory !== 'function')
+    throw new Error(`'${toModuleKey}' of '${toModuleName}' does not exports a function.`);
 
-  const toWriter = await toWriterInitializer.apply(undefined, ensureArray(to.args));
+  const toWriter = await toFactory.apply(undefined, ensureArray(toArgs));
   if (typeof toWriter !== 'function')
-    throw new Error(`'route.to.writer' of '${to.writer}' does not provide a function after initialization.`);
+    throw new Error(`'${toModuleKey}' of '${toModuleName}' does not provide a function after initialization.`);
 
   const controllerFn = typeof controller === 'string' ? importCliModule(controller) : undefined;
   if (controllerFn && typeof controllerFn !== 'function')
@@ -126,27 +138,27 @@ function routeFromFile(file: string): Promise<Route[]> {
 /**
  * Reads configuration provided by the cli.
  *
- * @param fromReader Package name of the reader.
+ * @param fromModule Package name of the reader.
  * @param fromArgs Args passed to the reader factory.
- * @param toWriter Package name of the writer.
+ * @param toModule Package name of the writer.
  * @param toArgs Args passed to the writer factory.
  * @param context Context params of the controller.
  * @returns Route definitions.
  */
 async function routeFromArgs(
-  fromReader: string,
+  fromModule: string,
   fromArgs: string,
-  toWriter: string,
+  toModule: string,
   toArgs: string,
   context: Record<string, unknown>
 ): Promise<Route[]> {
   return [await prepareRoute({
     from: {
-      reader: fromReader,
+      module: fromModule,
       args: fromArgs,
     },
     to: {
-      writer: toWriter,
+      module: toModule,
       args: toArgs,
     },
     controller: controller,
@@ -155,7 +167,7 @@ async function routeFromArgs(
 }
 
 const opts = program.opts();
-const { config, fromReader, fromArgs, toWriter, toArgs, controller, context } = opts;
+const { config, from, fromArgs, to, toArgs, controller, context } = opts;
 
 (async () => {
   let fromArgsParsed = undefined;
@@ -185,7 +197,7 @@ const { config, fromReader, fromArgs, toWriter, toArgs, controller, context } = 
     routes = await routeFromFile(config);
   } else if (Object.keys(opts).length > 0) {
     // from command line options
-    routes = await routeFromArgs(fromReader, fromArgsParsed, toWriter, toArgsParsed, contextParsed);
+    routes = await routeFromArgs(from, fromArgsParsed, to, toArgsParsed, contextParsed);
   } else {
     program.help();
   }
